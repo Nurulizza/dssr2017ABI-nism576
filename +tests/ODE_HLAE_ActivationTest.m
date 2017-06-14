701f951489d0584bb56a49f0b400bb87caab9e48
Nurul Izza <nism576@aucklanduni.ac.nz>
2017-06-14 12:02:56 +1200

    Added more test for ODE

--- +tests/ODE_HLAE_ActivationTest.m
+++ +tests/ODE_HLAE_ActivationTest.m
@@ -16,5 +16,30 @@ classdef ODE_HLAE_ActivationTest  < matlab.unittest.TestCase
             exp_dxdt_1 = 63900;
             testCase.verifyEqual(dxdt(1),exp_dxdt_1)
         end
+        function testdxdtTwo(testCase)
+            prm = [2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+            X = [4 3.2 767 0 0 0 0 0 0 0 0 0 0 0];
+            unused = 0;
+            [dxdt] = ODE_HLAE_Activation(unused, X, prm);
+            exp_dxdt_2 = 63900;
+            testCase.verifyEqual(dxdt(2),exp_dxdt_2)
+        end
+        function testdxdtThree(testCase)
+            prm = [2 3 2 3 2 0 0 0 0 0 0 0 0 0 0 0 ];
+            X = [4 3.2 767 0 0 0 5.6 8.2 0 0 0 14.7 0];
+            unused = 0;
+            [dxdt] = ODE_HLAE_Activation(unused, X, prm);
+            exp_dxdt_3 = -1184970;
+            testCase.verifyEqual(dxdt(3),exp_dxdt_3)
+        end
+        function testdxdtFour(testCase)
+            prm = [2 3 2 3 2 1 1 2 1 3 0 0 0 0 0 0 ];
+            X = [4 3.2 767 56.7 2 0 5.6 8.2 0 0 10 14.7 22];
+            unused = 0;
+            [dxdt] = ODE_HLAE_Activation(unused, X, prm);
+            exp_dxdt_4 = -15268;
+
+            testCase.verifyEqual(dxdt(4),exp_dxdt_4)
+        end
     end
 end
